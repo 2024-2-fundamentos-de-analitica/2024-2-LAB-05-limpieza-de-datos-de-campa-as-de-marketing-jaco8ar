6a1ec8ae0e6dbc9728a0e9d0e624bda457cf3c2f
Jacobo Ochoa <jochoara@unal.edu.co>
2025-02-21 10:38:43 -0500

    tarea 5

--- homework/homework.py
+++ homework/homework.py
@@ -3,54 +3,84 @@ Escriba el codigo que ejecute la accion solicitada.
 """
 
 # pylint: disable=import-outside-toplevel
+import pandas as pd
+import zipfile
+
+def readfiles():
+    files_dfs = []
+
+    for i in range(10):
+        zip_path = f"files/input/bank-marketing-campaing-{i}.csv.zip"
+        with zipfile.ZipFile(zip_path, 'r') as zip_file:
+            file_names = zip_file.namelist()
+            with zip_file.open(file_names[0]) as file:
+                file_df = pd.read_csv(file)
+                files_dfs.append(file_df)
+    return pd.concat(files_dfs)
+
+def define_client_df(df):
+    client_df = df[[
+        "client_id",
+        "age",
+        "job",
+        "marital",
+        "education",
+        "credit_default",
+        "mortgage"
+        ]].copy()
+
+    client_df["job"] = client_df["job"].str.replace(".", "").str.replace("-", "_")
+    client_df["education"] = client_df["education"].str.replace(".", "_").replace({"unknown": pd.NA})
+    
+    client_df["credit_default"] = client_df["credit_default"].map(lambda x: 1 if x == "yes" else 0)
+    client_df["mortgage"] = client_df["mortgage"].map(lambda x: 1 if x == "yes" else 0)
+
+    return client_df
+
+def define_campaign_df(df):
+    campaign_df = df[[
+        "client_id",        
+        "number_contacts",        
+        "contact_duration",        
+        "previous_campaign_contacts",        
+        "previous_outcome",
+        "campaign_outcome"
+    ]].copy()
+
+    campaign_df["previous_outcome"] = campaign_df["previous_outcome"].map(lambda x: 1 if x == "success" else 0)    
+    campaign_df["campaign_outcome"] = campaign_df["campaign_outcome"].map(lambda x: 1 if x == "yes" else 0)    
+
+    month_mapping = {
+    "jan": "01", "feb": "02", "mar": "03", "apr": "04", "may": "05", "jun": "06",
+    "jul": "07", "aug": "08", "sep": "09", "oct": "10", "nov": "11", "dec": "12"
+    }
+
+    df["month"] = df["month"].map(month_mapping)
+
+    campaign_df["last_contact_date"] = "2022-" + df["month"] + "-" + df["day"].astype(str).str.zfill(2)
+
+    return campaign_df
+
+def define_economics_df(df):
+    economics_df = df[[
+    "client_id",
+    "cons_price_idx",
+    "euribor_three_months"
+    ]].copy()
+
+    return economics_df
 
 
 def clean_campaign_data():
-    """
-    En esta tarea se le pide que limpie los datos de una campaña de
-    marketing realizada por un banco, la cual tiene como fin la
-    recolección de datos de clientes para ofrecerls un préstamo.
+    
+    df = readfiles()
+    client_df = define_client_df(df)
+    campaign_df = define_campaign_df(df)
+    economics_df = define_economics_df(df)
 
-    La información recolectada se encuentra en la carpeta
-    files/input/ en varios archivos csv.zip comprimidos para ahorrar
-    espacio en disco.
-
-    Usted debe procesar directamente los archivos comprimidos (sin
-    descomprimirlos). Se desea partir la data en tres archivos csv
-    (sin comprimir): client.csv, campaign.csv y economics.csv.
-    Cada archivo debe tener las columnas indicadas.
-
-    Los tres archivos generados se almacenarán en la carpeta files/output/.
-
-    client.csv:
-    - client_id
-    - age
-    - job: se debe cambiar el "." por "" y el "-" por "_"
-    - marital
-    - education: se debe cambiar "." por "_" y "unknown" por pd.NA
-    - credit_default: convertir a "yes" a 1 y cualquier otro valor a 0
-    - mortage: convertir a "yes" a 1 y cualquier otro valor a 0
-
-    campaign.csv:
-    - client_id
-    - number_contacts
-    - contact_duration
-    - previous_campaing_contacts
-    - previous_outcome: cmabiar "success" por 1, y cualquier otro valor a 0
-    - campaign_outcome: cambiar "yes" por 1 y cualquier otro valor a 0
-    - last_contact_day: crear un valor con el formato "YYYY-MM-DD",
-        combinando los campos "day" y "month" con el año 2022.
-
-    economics.csv:
-    - client_id
-    - const_price_idx
-    - eurobor_three_months
-
-
-
-    """
-
-    return
+    client_df.to_csv("files/output/client.csv", index = False)
+    campaign_df.to_csv("files/output/campaign.csv", index = False)
+    economics_df.to_csv("files/output/economics.csv", index = False)
 
 
 if __name__ == "__main__":
